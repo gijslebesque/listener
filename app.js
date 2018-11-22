37db0e80ebbfdeca9719b597e4c7efad16a2d297
Gijs Lebesque <gijslebesque@gmail.com>
2018-11-22 21:21:05 +0100

    ping

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const app = express();
+const os = require('os');
+const ipAddress = os.networkInterfaces().en0[0].address;
+console.log(ipAddress)
+
 let {PythonShell} = require('python-shell')
 
 
-app.get("/", (req, res) =>{
+// app.get("/", (req, res) =>{
 
-  let options = {
-    pythonOptions: ['-u'], // get print results in real-time
-  };
+//   let options = {
+//     pythonOptions: ['-u'], // get print results in real-time
+//   };
    
-  PythonShell.run('listener.py', options, (err, results) => {
-    if (err) throw err;
-    // results is an array consisting of messages collected during execution
-    res.send(results)
-  });
+//   PythonShell.run('listener.py', options, (err, results) => {
+//     if (err) throw err;
+//     // results is an array consisting of messages collected during execution
+//     res.send(results)
+//   });
+// })
 
 
 
+
+app.get("/ping", (req, res) =>{
+ 
+  res.send("pong");
 })
 
-app.listen(4040, ()=>{
+app.listen(3000, ipAddress, ()=>{
     console.log("lsitening")
 })
